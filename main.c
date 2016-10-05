99b590cf15746a886a6dca38a093c2a490eeb701
maciejspychala <mac.spychala@gmail.com>
2016-10-05 09:54:55 +0200

    code format

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include <signal.h>
 #include "structs.h"
 
-//
 static struct sembuf buf;
 int serverId;
 int *settings;
@@ -28,12 +27,12 @@ void setSem(int key) {
         exit(1);
     }
     if (semctl(semid, 1, SETVAL, (int) 1) == -1) {
-        perror("Nadanie wartosci semaforowi 0");
+        perror("Nadanie wartosci semaforowi 1");
         exit(1);
     }
 }
 
-void podnies(unsigned short int semnum) {
+void pickUpSem(unsigned short int semnum) {
     buf.sem_num = semnum;
     buf.sem_op = 1;
     buf.sem_flg = 0;
@@ -44,7 +43,7 @@ void podnies(unsigned short int semnum) {
 
 }
 
-void opusc(unsigned short int semnum) {
+void lowerSem(unsigned short int semnum) {
     buf.sem_num = semnum;
     buf.sem_op = -1;
     buf.sem_flg = 0;
@@ -55,36 +54,36 @@ void opusc(unsigned short int semnum) {
     }
 }
 
-int zyje() {
-    int lol;
-    opusc(0);
-    lol = settings[1];
-    podnies(0);
-    return lol;
+int clientAlive() {
+    int alive;
+    lowerSem(0);
+    alive = settings[1];
+    pickUpSem(0);
+    return alive;
 }
 
-int wyswietlac() {
-    int lol;
-    opusc(0);
-    lol = settings[0];
-    podnies(0);
-    return lol;
+int shouldDisplay() {
+    int display;
+    lowerSem(0);
+    display = settings[0];
+    pickUpSem(0);
+    return display;
 }
 
-void setWyswietlac(int lol) {
-    opusc(0);
-    settings[0] = lol;
-    podnies(0);
+void setDisplay(int display) {
+    lowerSem(0);
+    settings[0] = display;
+    pickUpSem(0);
 }
 
-void setZyje(int lol) {
-    opusc(0);
-    settings[1] = lol;
-    podnies(0);
+void setClientAlive(int alive) {
+    lowerSem(0);
+    settings[1] = alive;
+    pickUpSem(0);
 }
 
 
-void pokaData(struct Data data, char *info[3]) {
+void showInfo(struct Data data, char **info) {
     printf("Nacisnij 'a' aby atakowac, 'b' aby budowac, 'k' aby zakonczyc\n");
     printf("+-----------------+---------+\n");
     printf("| lekka piechota  | %6d |\n", data.light);
@@ -107,8 +106,8 @@ void pokaData(struct Data data, char *info[3]) {
     }
 }
 
-void atak() {
-    setWyswietlac(0);
+void startAttack() {
+    setDisplay(0);
     printf("Przeprowadzasz atak!\n");
     printf("Ile wyslac l. piechoty ");
     scanf("%d", &attack.light);
@@ -116,13 +115,13 @@ void atak() {
     scanf("%d", &attack.heavy);
     printf("Ile wyslac jazdy ");
     scanf("%d", &attack.cavalry);
-    setWyswietlac(1);
+    setDisplay(1);
     msgsnd(serverId, &attack, sizeof(attack) - sizeof(long), 0);
 }
 
 
-void buduj() {
-    setWyswietlac(0);
+void startBuilding() {
+    setDisplay(0);
     printf("Ile wybudowac l. piechoty ");
     scanf("%d", &build.light);
     printf("Ile wybudowac c. piechoty ");
@@ -131,8 +130,8 @@ void buduj() {
     scanf("%d", &build.cavalry);
     printf("Ile wybudowac robotnikow ");
     scanf("%d", &build.workers);
-    setWyswietlac(1);
-    msgsnd(serverId, &build, sizeof(build)  - sizeof(long), 0);
+    setDisplay(1);
+    msgsnd(serverId, &build, sizeof(build) - sizeof(long), 0);
 }
 
 void setTypes() {
@@ -149,20 +148,20 @@ int main() {
 
     int key = 15071410;
 
-    int kolejkaInitId = msgget(key, IPC_CREAT | 0640);
+    int initMsgID = msgget(key, IPC_CREAT | 0640);
 
-    if (kolejkaInitId == -1) {
+    if (initMsgID == -1) {
         perror("Utworzenie kolejki komunikatow");
         exit(1);
     }
     printf("czekam na serwer\n");
-    while (msgrcv(kolejkaInitId, &initBuf, sizeof(initBuf.nextMsg), 1, 0) == -1) {
-        printf("lolxdlolol");
+    while (msgrcv(initMsgID, &initBuf, sizeof(initBuf.nextMsg), 1, 0) == -1) {
+        printf("czekam");
     }
     printf("serwer wlaczony, id kolejki to %d\n", initBuf.nextMsg);
     setSem(initBuf.nextMsg);
     initBuf.mtype = 2;
-    msgsnd(kolejkaInitId, &initBuf, sizeof(initBuf.nextMsg), 0);
+    msgsnd(initMsgID, &initBuf, sizeof(initBuf.nextMsg), 0);
     int p = shmget(initBuf.nextMsg + 20, 2 * sizeof(int), IPC_CREAT | 0640);
     settings = (int *) shmat(p, NULL, 0);
     printf("czekam na drugiego gracza\n");
@@ -184,35 +183,32 @@ int main() {
         infos[1] = "  ";
         infos[2] = "  ";
         data.end = 0;
-        int koniec = 0;
-        while (!koniec) {
+        int theEnd = 0;
+        while (!theEnd) {
             msgrcv(serverId, &data, sizeof(data) - sizeof(long), 1, 0);
             settings = (int *) shmat(p, NULL, 0);
-            if (wyswietlac()) {
+            if (shouldDisplay()) {
                 system("clear");
-                pokaData(data, infos);
+                showInfo(data, infos);
             }
-            koniec = (int) data.end;
+            theEnd = (int) data.end;
         }
-    }
-    else if (fork() == 0) {
+    } else if (fork() == 0) {
         while (1) {
-            msgsnd(serverId, &alive, sizeof(alive)  - sizeof(long), 0);
+            msgsnd(serverId, &alive, sizeof(alive) - sizeof(long), 0);
             sleep(2);
         }
-    }
-    else if (fork() == 0) {
-        int recive;
+    } else if (fork() == 0) {
+        int receive;
         int howMany = 0;
         msgrcv(serverId, &alive, sizeof(alive) - sizeof(long), 5, 0);
         while (howMany < 3) {
             sleep(2);
-            recive = msgrcv(serverId, &alive, sizeof(alive) - sizeof(long), 5, IPC_NOWAIT);
-            if (recive < 0) {
+            receive = (int) msgrcv(serverId, &alive, sizeof(alive) - sizeof(long), 5, IPC_NOWAIT);
+            if (receive < 0) {
                 ++howMany;
                 printf("serwer nie odpowiada, laczymy ponownie\n");
-            }
-            else {
+            } else {
                 howMany = 0;
             }
             if (howMany > 2) {
@@ -223,17 +219,16 @@ int main() {
         }
         exit(0);
 
-    }
-    else {
-        while (zyje()) {
+    } else {
+        while (clientAlive()) {
             char l;
             l = getchar();
             if (l == 'b' || l == 'B')
-                buduj();
+                startBuilding();
             if (l == 'a' || l == 'A')
-                atak();
+                startAttack();
             if (l == 'k' || l == 'K')
-                setZyje(0);
+                setClientAlive(0);
         }
     }
     shmctl(p, IPC_RMID, 0);
